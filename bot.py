2ad04fa82f0c3443ac37e7dab70187f8f91e5755
virez001 <137012240+virez001@users.noreply.github.com>
2024-01-19 21:22:58 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,7 @@ def create_progress_bar_image():
 
     draw.rectangle([0, 0, progress_percentage * 4, 40], fill='green')
 
-    draw.text((10, 10), f"EL   PROGRESO   DE   {current_date.year}   es:   {progress_percentage:.2f} %   complete", fill='black')
+    draw.text((10, 10), f"EL   PROGRESO   DE   {current_date.year}   ES   DE   {progress_percentage:.2f} %", fill='black')
 
 
     image.save("progress_bar.jpg")  # Cambia la ruta del archivo si es necesario
